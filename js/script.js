633014532d05b297ad25e06f75d9eaeed9f443e7
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-02-23 14:42:02 +0600

    23/02/2023

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 
 function HTMLtoPDF() {
-    const input = document.getElementById('html_to_pdf');    
-html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('p', 'pt', 'A4');
-        pdf.addImage(imgData, 'PNG', 2, 5);
-        pdf.save('download.pdf');  
-  });
+//     const input = document.getElementById('html_to_pdf');    
+// html2canvas(input)
+//       .then((canvas) => {
+//         const imgData = canvas.toDataURL('image/png');
+//         const pdf = new jsPDF('p', 'pt', 'A4');
+//         var width = pdf.internal.pageSize.getWidth();
+//         var height = pdf.internal.pageSize.getHeight();
+//         pdf.addImage(imgData, 'PNG', 2, 5, width, height);
+//         pdf.save('download.pdf');  
+//   });
+
+
+var doc = new jsPDF('p', 'pt', 'A4');
+        var margin = 10;
+        var scale = .76;
+        // doc.setFont('Lato-Regular', 'normal');
+        doc.html(document.getElementById('html_to_pdf'), {
+        x: margin,
+        y: margin,
+        setFont: 'courier',
+            html2canvas: {
+                setFont: 'courier',
+                scale: scale,
+            },
+            callback: function(doc){
+                // doc.setFont('courier'),
+                // doc.setFontType('normal'),
+                doc.output('dataurlnewwindow', 
+                {filename: 'fichero-pdf. pdf'});
+            }
+        });
         // var doc = new jsPDF('p', 'pt', 'A4');
         // var margin = 10;
         // var scale = (doc.internal.pageSize.width - margin * 2) /
